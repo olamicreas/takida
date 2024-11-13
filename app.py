5219c18499d75a5742a16be6bfafdddce858c3d5
olamicreas <abdulquayyumoyedotun@gmail.com>
2024-11-13 23:56:34 +0100

    sec

--- app.py
+++ app.py
@@ -9,8 +9,9 @@ app.secret_key = 'your_secret_key_here'
 # Flask-Mail configuration
 app.config['MAIL_SERVER'] = 'smtp.gmail.com'
 app.config['MAIL_PORT'] = 465
-app.config['MAIL_USERNAME'] = 'takidamakeup@gmail.com'
-app.config['MAIL_PASSWORD'] = 'xumzbbrrbacnkyvj'
+app.config['MAIL_USERNAME'] = os.getenv('MAIL_USERNAME')
+app.config['MAIL_PASSWORD'] = os.getenv('MAIL_PASSWORD')
+
 app.config['MAIL_DEFAULT_SENDER'] = app.config['MAIL_USERNAME']
 app.config['MAIL_USE_TLS'] = False
 app.config['MAIL_USE_SSL'] = True
